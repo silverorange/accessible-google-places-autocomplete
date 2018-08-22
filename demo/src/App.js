e0e484657fc2a1131be173e79b757a2fa30ea0e6
Michael Gauthier <mike@silverorange.com>
2018-08-22 15:51:17 -0300

    Improve autocomplete demo
     - no longer use console.log
     - display selected address

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,43 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AccessibleGooglePlacesAutocomplete } from 'accessible-google-places-autocomplete';
+import AddressView from './AddressView';
 
-function App() {
-  document.title = 'Accessible Google Places Address Autocomplete';
+const googlePlacesOptions = {
+  componentRestrictions: { country: 'CA' },
+  types: ['geocode']
+};
 
-  function onConfirm(place) {
-    console.log(place);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: []
+    };
   }
 
-  function onClear() {
-    console.log('selection was cleared');
-  }
-
-  const googlePlacesOptions = {
-    componentRestrictions: { country: 'CA' },
-    types: ['geocode']
+  onConfirm = place => {
+    this.setState(_ => ({
+      address: place.address_components
+    }));
   };
 
-  return (
-    <div className="App">
-      <form>
-        <label htmlFor="address_input" aria-describedby="address_note">
-          What is your mailing address?
-        </label>
-        <div id="address_note" style={{ display: 'none' }}>
-          This is an automatic address auto-complete. Use arrow keys to choose
-          options.
-        </div>
-        <AccessibleGooglePlacesAutocomplete
-          id="address_input"
-          googlePlacesApiKey={process.env.REACT_APP_GOOGLE_PLACES_API_KEY}
-          googlePlacesOptions={googlePlacesOptions}
-          onConfirm={onConfirm}
-          onClear={onClear}
-          useMoreAccuratePostalCode
-        />
-      </form>
-    </div>
-  );
-}
+  onClear = () => {
+    this.setState(_ => ({
+      address: []
+    }));
+  };
 
-export default App;
+  render() {
+    const { address } = this.state;
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="address_input" aria-describedby="address_note">
+            What is your mailing address?
+          </label>
+          <div id="address_note" style={{ display: 'none' }}>
+            This is an automatic address auto-complete. Use arrow keys to choose
+            options.
+          </div>
+          <AccessibleGooglePlacesAutocomplete
+            id="address_input"
+            googlePlacesApiKey={process.env.REACT_APP_GOOGLE_PLACES_API_KEY}
+            googlePlacesOptions={googlePlacesOptions}
+            onConfirm={this.onConfirm}
+            onClear={this.onClear}
+            useMoreAccuratePostalCode
+          />
+        </form>
+        <AddressView address={address} />
+      </div>
+    );
+  }
+}
