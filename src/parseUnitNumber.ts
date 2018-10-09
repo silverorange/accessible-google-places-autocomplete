1ea96e4216a00174b50e74480fa770ddc96131d4
Michael Gauthier <mike@silverorange.com>
2018-10-09 15:19:00 -0300

    Parse unit numbers after street address

--- src/parseUnitNumber.ts
+++ src/parseUnitNumber.ts
@@ -6,7 +6,7 @@ export interface IParseUnitNumberResult {
 
 export function parseUnitNumber(query: string): IParseUnitNumberResult {
   // Match dashed address formats where the unit goes before the street number.
-  const dashedMatches = /^\s*([0-9]+|[a-z])[\s-–]+([0-9]+\s.*)\s*$/i.exec(
+  const dashedMatches = /^[\s#]*([0-9]+|[a-z])[\s-–]+([0-9]+\s.*)\s*$/i.exec(
     query
   );
   if (dashedMatches !== null) {
@@ -24,6 +24,42 @@ export function parseUnitNumber(query: string): IParseUnitNumberResult {
       civicAddress: `${dashedLetterMatches[1]} ${dashedLetterMatches[3]}`,
       unitDesignator: '',
       unitNumber: dashedLetterMatches[2]
+    };
+  }
+
+  const designators =
+    '(apt\\.?|apartment|bldg\\.?|building|dept\\.?|department|fl\\.?|floor|rm\\.?|room|ste\\.?|suite|unit)';
+
+  const normalizedDesignators = {
+    apt: 'apartment',
+    bldg: 'building',
+    dept: 'department',
+    fl: 'floor',
+    rm: 'room',
+    ste: 'suite'
+  };
+
+  const afterStreet = new RegExp(
+    `^\\s*([0-9]+.+)\\s${designators}\\s*([0-9]+|[a-z])(.*)$`,
+    'i'
+  );
+  const afterStreetMatches = afterStreet.exec(query);
+  if (afterStreetMatches !== null) {
+    const designator = afterStreetMatches[2]
+      .replace(/\./, '')
+      .toLocaleLowerCase();
+
+    const normalizedDesignator = normalizedDesignators[designator]
+      ? normalizedDesignators[designator]
+      : designator;
+
+    return {
+      civicAddress: `${afterStreetMatches[1].replace(
+        /[\s,]*$/,
+        ''
+      )}, ${afterStreetMatches[4].replace(/^[\s,]*/, '')}`,
+      unitDesignator: normalizedDesignator,
+      unitNumber: afterStreetMatches[3]
     };
   }
 
