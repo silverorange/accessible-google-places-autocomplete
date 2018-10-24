207f15545c64104a9515ffc37d64520bfe34cdf9
Nick Burka <nick@silverorange.com>
2018-10-24 14:48:32 -0400

    better padding on language controls

--- demo/src/App.js
+++ demo/src/App.js
@@ -50,7 +50,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <form>
-          <div>
+          <div className="languageControls">
             <label>
               <input
                 type="radio"
